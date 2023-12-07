79aefd999c990234914efea07ae365809c8b498c
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-12-07 15:00:57 +0200

    Fixed issue mentioned in comment #discussion_r1417862908_

---
+++ src/containers/App.tsx
@@ -0,0 +1,13 @@
+import TopRatedMovieCardList from '../components/TopRatedMovieCardList/TopRatedMovieCardList';
+import PopularMovieCardList from '../components/PopularMovieCardList/PopularMovieCardList';
+
+function App() {
+    return (
+        <div>
+            <TopRatedMovieCardList />
+            <PopularMovieCardList />
+        </div>
+    );
+}
+
+export default App;
